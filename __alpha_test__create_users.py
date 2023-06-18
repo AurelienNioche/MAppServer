8e3a2d81bcbf1a54a69eb6dc4417450ac67ce565
AurelienNioche <nioche.aurelien@gmail.com>
2023-06-18 18:52:35 +0100

    update

--- __alpha_test__create_users.py
+++ __alpha_test__create_users.py
@@ -8,8 +8,8 @@ from test_user__create import test_user_xp_like__create
 
 
 if __name__ == "__main__":
-    experiment_name = "alpha-test-2"
-    test_user_xp_like__create(username="mich-alph2", seed=123, experiment_name=experiment_name)
-    test_user_xp_like__create(username="stuA-alph2", seed=1234, experiment_name=experiment_name)
-    test_user_xp_like__create(username="stuB-alph2", seed=12345, experiment_name=experiment_name)
+    experiment_name = "alpha-test-3"
+    test_user_xp_like__create(username="mich-alph3", seed=123, experiment_name=experiment_name)
+    # test_user_xp_like__create(username="stuA-alph2", seed=1234, experiment_name=experiment_name)
+    # test_user_xp_like__create(username="stuB-alph2", seed=12345, experiment_name=experiment_name)
 
