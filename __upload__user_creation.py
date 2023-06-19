ebfd8e53fddabcd029cf184d5784171aae65c6be
AurelienNioche <nioche.aurelien@gmail.com>
2023-06-19 18:18:11 +0100

    update

--- __upload__user_creation.py
+++ __upload__user_creation.py
@@ -1,5 +1,5 @@
 import os
 
 # scp local_file user@remote_host:remote_file
-out = os.system("scp data/user_creation/rewards.csv aurelien@pearse.dcs.gla.ac.uk:data/user_creation/rewards.csv")
+out = os.system("scp data/user_creation/rewards.csv aurelien@pearse.dcs.gla.ac.uk:DjangoApps/MAppServer/data/user_creation/rewards.csv")
 print(out)
