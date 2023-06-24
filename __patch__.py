542df2199a0412d35bfaa9311d3f18dd5b497d02
AurelienNioche <nioche.aurelien@gmail.com>
2023-06-24 13:41:18 +0100

    update

--- __patch__.py
+++ __patch__.py
@@ -4,23 +4,23 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE",
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-from user.models import User
+from user.models import User, Reward, Status
 
-username = "242K"
-u = User.objects.filter(username=username).first()
-if u is not None:
-    print(f"I found user {u.username}")
-
-else:
-    print(f"I did not found user {username}")
-
-
-username = "2.80E+02"
+username = "302M"
 u = User.objects.filter(username=username).first()
 if u is not None:
     print(f"I found user {username}")
-    u.username = "28E1"
-    u.save()
+    status = Status.objects.filter(user=u).first()
+    for k in ["last_update_dt", "chest_amount", "daily_objective", "state", "reward_id", "starting_at", "objective",
+              "amount", "day_of_the_week", "day_of_the_month", "month", "step_number", "reward_id", "error"]:
+        print(f"{k}: {getattr(status, k)}")
+
+    rewards = Reward.objects.filter(user=u).order_by("date", "objective")
+    for r in rewards:
+        to_print = f"r {r.id}: {r.date} {r.objective} {r.amount}"
+        if r.objective_reached:
+            to_print += f"{r.objective_reached_dt}"
+        print(to_print)
 
 else:
     print(f"I did not found user {username}")
