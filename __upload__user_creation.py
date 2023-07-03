44e1d02d37cdaf9acab1fdef0d9e3cd938b78d74
AurelienNioche <nioche.aurelien@gmail.com>
2023-07-03 19:58:23 +0100

    update

--- __upload__user_creation.py
+++ __upload__user_creation.py
@@ -3,7 +3,7 @@ import os
 from MAppServer.settings import USER_CREATION_CSV_PATH, SERVER_USER, SERVER_DOMAIN, SERVER_BASE_DIR
 
 # scp local_file user@remote_host:remote_file
-out = os.system(f"scp -r {USER_CREATION_CSV_PATH} {SERVER_USER}@{SERVER_DOMAIN}:{SERVER_BASE_DIR}/{USER_CREATION_CSV_PATH}")
+out = os.system(f"scp {USER_CREATION_CSV_PATH}/* {SERVER_USER}@{SERVER_DOMAIN}:{SERVER_BASE_DIR}/{USER_CREATION_CSV_PATH}")
 if out == 0:
     print("Files successfully uploaded.")
 else:
