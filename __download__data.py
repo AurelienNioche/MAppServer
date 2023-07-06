39d64f4a2bd405610975594e2a97194ed6ebcedd
AurelienNioche <nioche.aurelien@gmail.com>
2023-07-06 16:57:32 +0100

    update

---
+++ __download__data.py
@@ -0,0 +1,10 @@
+import os
+
+from MAppServer.settings import USER_CREATION_CSV_PATH, SERVER_USER, SERVER_DOMAIN, SERVER_BASE_DIR
+
+# scp local_file user@remote_host:remote_file
+out = os.system(f"scp {USER_CREATION_CSV_PATH} {SERVER_USER}@{SERVER_DOMAIN}:{SERVER_BASE_DIR}/{USER_CREATION_CSV_PATH}")
+if out == 0:
+    print("File successfully uploaded.")
+else:
+    print("Error while uploading file.")
